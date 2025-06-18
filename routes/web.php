82a7cb338b46428cab80344617e73fa08f61eb68
LuminousV Windows <farelreyhan6@gmail.com>
2025-06-18 22:23:52 +0700

    dev: create AnggotaController and add views anggota

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,17 @@ use App\Http\Controllers\HelloWorldController;
 use App\Http\Controllers\HtmlController;
 use App\Http\Controllers\LatihanController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnggotaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("helloworld", [HelloWorldController::class,"index"]);
-Route::get("ambilfile", [HelloWorldController::class,"ambilFile"]);
-Route::get("getlorem", [HtmlController::class,"getLorem"]);
-Route::get("table-mahasiswa", [LatihanController::class,"getTable"]);
-Route::get("form-mahasiswa", [LatihanController::class,"getForm"]);
+Route::get("helloworld", [HelloWorldController::class, "index"]);
+Route::get("ambilfile", [HelloWorldController::class, "ambilFile"]);
+Route::get("getlorem", [HtmlController::class, "getLorem"]);
+Route::get("table-mahasiswa", [LatihanController::class, "getTable"]);
+Route::get("form-mahasiswa", [LatihanController::class, "getForm"]);
+
+// New
+Route::resource("anggota", AnggotaController::class);
